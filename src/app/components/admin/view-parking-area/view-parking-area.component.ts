9008091af02343575342174a74612b7b7543b7b0
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 14:30:59 +0500

    parking slots are shown to admin but not full flaged

--- src/app/components/admin/view-parking-area/view-parking-area.component.ts
+++ src/app/components/admin/view-parking-area/view-parking-area.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'app-view-parking-area',
-  templateUrl: './view-parking-area.component.html',
-  styleUrls: ['./view-parking-area.component.css']
+    selector: 'app-view-parking-area',
+    templateUrl: './view-parking-area.component.html',
+    styleUrls: ['./view-parking-area.component.css']
 })
 export class ViewParkingAreaComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private fs: FireService,
+        private router: Router,
+        private ActivatedRoute: ActivatedRoute,
+    ) { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.ActivatedRoute.params.subscribe((data: any) => {
+            console.log("Router params: ", data)
+            this.parking = this.fs.getData("parkings/" + data.uid)
+            console.log(this.parking);
+            this.slots = this.fs.getList("parkings/" + data.uid + "/slots")
+        });
+    }
+    parking;
+    slots;
 }
