2dcb6920ce2ecb73af61c3a661e72ac7ba65ee1e
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 09:36:10 +0500

    firebase connection added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,32 @@ import { HttpModule } from '@angular/http';
 import { NgReduxModule } from 'ng2-redux';
 import { StoreModule } from './store'
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-ThotQG3b2xag7xs07VGoK5s2wN9Vrqw",
+    authDomain: "parking-3dbe2.firebaseapp.com",
+    databaseURL: "https://parking-3dbe2.firebaseio.com",
+    storageBucket: "parking-3dbe2.appspot.com",
+    messagingSenderId: "1001157393966"
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    NgReduxModule,
-    StoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        NgReduxModule,
+        StoreModule,
+        AngularFireModule.initializeApp(firebaseConfig)
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
