d9ea84592d45a63787394e047ecdd51026af81d0
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 12:00:07 +0500

    add parking area done, admin can now add a parking area in application

--- src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-admin-dashboard',
-  templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+    selector: 'app-admin-dashboard',
+    templateUrl: './admin-dashboard.component.html',
+    styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    parking = {
+        name: "",
+        slots: [],
+        slotCount: 5
+    }
+
+    addParkingArea() {
+
+        for (let i = 0; i < this.parking.slotCount; i++) {
+            this.parking.slots[i] = true;
+        }
+
+
+        this.fs.pushData("parkings", this.parking)
+            .catch(error => {
+                console.log("Error is: ", error);
+            })
+            .then(data => {
+                //this.router.navigate(['/login']);
+            });
+
+    }
 
 }
