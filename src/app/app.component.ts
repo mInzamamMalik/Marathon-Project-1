bbfa549f52310887b3c1d05ec4d136decff1e3a8
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 17:24:02 +0500

    email service included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+declare let emailjs: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Parking System!';
+    title = 'Parking System!';
+    ngOnInit() {
+        emailjs.send("gmail", "abc", {
+            name: "James",
+            notes: "Check this out!",
+            email: "a@a.com",
+            content: "this is text of email"
+
+        }).then(function (response) {
+            console.log("SUCCESS", response);
+        }, function (error) {
+            console.log("FAILED", error);
+        });
+    }
 }
