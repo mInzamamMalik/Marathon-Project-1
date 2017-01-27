6b7fd9b19612cba2afe936d5131f2f7f683f45ab
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 17:31:40 +0500

    real time parking areas list displayed to user

--- src/app/components/user/user-dashboard/user-dashboard.component.ts
+++ src/app/components/user/user-dashboard/user-dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-user-dashboard',
-  templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.css']
+    selector: 'app-user-dashboard',
+    templateUrl: './user-dashboard.component.html',
+    styleUrls: ['./user-dashboard.component.css']
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.parkings = this.fs.getList('parkings');
+    }
+    parkings;
 
 }
