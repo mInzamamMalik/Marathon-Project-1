a0957cd89053e712e129ad0255088c18565c88e4
malikasinger1 <malikasinger1@gmail.com>
2017-01-27 09:41:54 +0500

    signup and login function added to fire service

--- src/app/providers/fire.service.ts
+++ src/app/providers/fire.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
+import { PreloadingStrategy, Route, Router } from '@angular/router';
 
 @Injectable()
 export class FireService {
 
-  constructor() { }
+    constructor(private af: AngularFire, private router: Router) {
+        console.log("Fire Service initialized");
+    }
 
+    doLogin(email, password) {
+        return this.af.auth.login(
+            {
+                email: email,
+                password: password
+            },
+            {
+                provider: AuthProviders.Password,
+                method: AuthMethods.Password,
+            }).catch((error: any) => {
+                console.log(error);
+            })
+    }
+    doSignup(email, password) {
+        return this.af.auth.createUser({ email: email, password: password })            
+    }
 }
