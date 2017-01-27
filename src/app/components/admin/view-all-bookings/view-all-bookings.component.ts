9d6a9fd84331df68c9e67bce55b58be12e62567f
malikasinger1 <malikasinger1@gmail.com>
2017-01-28 00:39:50 +0500

    list of all bookings displayed to admin,  admin can cancel any of them any time

--- src/app/components/admin/view-all-bookings/view-all-bookings.component.ts
+++ src/app/components/admin/view-all-bookings/view-all-bookings.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-view-all-bookings',
-  templateUrl: './view-all-bookings.component.html',
-  styleUrls: ['./view-all-bookings.component.css']
+    selector: 'app-view-all-bookings',
+    templateUrl: './view-all-bookings.component.html',
+    styleUrls: ['./view-all-bookings.component.css']
 })
 export class ViewAllBookingsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService, ) { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.bookingsList = this.fs.getList('bookings')
+    }
+    bookingsList;
+    cancelReservation(item) {
+        if (confirm("Are you sure to delete reservation of " + item.parkingAreaName + " Slot: " + item.slot)) {
+            this.fs.setData("bookings/" + item.$key, null).then(deleted => {
+                console.log("reservation is cancelled: ", deleted);
+            })
+        }
+    }
 }
