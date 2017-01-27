fdb9261cad1f693967e25706a19059da1621de5e
malikasinger1 <malikasinger1@gmail.com>
2017-01-28 00:21:35 +0500

    list of bookings is displayed to user, user can cancel it and print it

--- src/app/components/user/viewbookings/viewbookings.component.ts
+++ src/app/components/user/viewbookings/viewbookings.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../../providers/fire.service';
 
 @Component({
-  selector: 'app-viewbookings',
-  templateUrl: './viewbookings.component.html',
-  styleUrls: ['./viewbookings.component.css']
+    selector: 'app-viewbookings',
+    templateUrl: './viewbookings.component.html',
+    styleUrls: ['./viewbookings.component.css']
 })
 export class ViewbookingsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fs: FireService, ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.fs.getUser()
+            .subscribe(auth => {
+                console.log("this is auth: ", auth);
+                this.bookingsList = this.fs.getList('bookings', {
+                    query: {
+                        orderByChild: 'userUid',
+                        equalTo: auth.uid
+                    }
+                })
+            });
+    }
+    bookingsList;
+    cancelReservation(item) {
+        if (confirm("Are you sure to delete reservation of " + item.parkingAreaName + " Slot: " + item.slot)) {
+            this.fs.setData("bookings/" + item.$key, null).then(deleted => {
+                console.log("reservation is cancelled: ", deleted);
+            })
+        }
+    }
+    print() {
+        window.print();
+    }
 
 }
